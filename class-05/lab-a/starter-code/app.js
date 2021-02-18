de8bbef2c1a7f54bde3a01a77583a4b78d650ad2
Amjad Mesmar <Amjad.Mesmar@gmail.com>
2021-02-18 20:20:02 +0200

    solved problem 2

--- class-05/lab-a/starter-code/app.js
+++ class-05/lab-a/starter-code/app.js
@@ -13,7 +13,6 @@ function sum(a, b) { //eslint-disable-line
 let theSum = a + b;
 
 let theSumString = 'The sum of ' + a + ' and ' + b + ' is ' + theSum + '.';
-//let theSumString = 'The sum of ${a} and ${b} is ${theSum}.';
 
 return [theSum,theSumString];
 
@@ -35,10 +34,16 @@ Test this function by hand in the console to get it working, and when you think 
 // Write your code here
 function multiply(a, b) { //eslint-disable-line
 
+let multiply = a * b;
+
+let multiplyString = 'The product of ' + a + ' and ' + b + ' is ' + multiply + '.';
+
+return [multiply,multiplyString];
+
 }
 
 // Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
+testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
